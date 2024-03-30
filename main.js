8348c4f30be170218a019eb645522311095bb691
Diffyyy <80896379+Diffyyy@users.noreply.github.com>
2024-03-30 17:46:01 +0800

    added glass layer

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ scene.add(light6)
 // let gg = BufferGeometryUtils.mergeBufferGeometries(geoms,true)
 // gg.applyMatrix4(clone.matrix.clone().invert());
 // gg.userData.materials = meshes.map(m=>m.material)
-// controls.panSpeed = 5
+/// controls.panSpeed = 5
 
 
 class Maze{
@@ -101,8 +101,11 @@ scene.add(maze.model)
 initializeInputHandler(maze, scene)
 
 /////////// PHYSICS ///////////
+// Physics world
 const world = new CANNON.World();
 world.gravity = new CANNON.Vec3(0, -1, -1)
+world.allowSleep = true; // improve performance
+world.defaultContactMaterial.friction = 1; 
 
 // Ball mesh
 const ballRadius = 0.025
@@ -119,7 +122,7 @@ const ballBody = new CANNON.Body({
     shape: new CANNON.Sphere(ballRadius),
     material: new CANNON.Material({
         friction: 0,
-        restitution: 0.5
+        restitution: 0
     }),
     mass: 1,
 })
@@ -129,8 +132,71 @@ ballBody.quaternion.set(ball.quaternion.x, ball.quaternion.y, ball.quaternion.z,
 // Add ball body to world after rendering wall bodiies
 world.addBody(ballBody);
 
+////////////// GLASS LAYER ///////////
+
+// Define dimensions for the glass cube
+console.log('Width of maze: ', maze.width)
+console.log('Height of maze: ', maze.height)
+console.log('Maze Wall thickness: ', maze.wall_thickness)
+console.log('Maze Cell Size: ', maze.cell_size)
+console.log('Wall Height: ', maze.height)
+console.log('Radius: ', maze.radiusPercent)
+
+// For now, might need to change later
+const glassCubeWidth = maze.width * maze.cell_size + 20 * maze.wall_thickness;
+const glassCubeHeight = maze.height * maze.cell_size + 20 * maze.wall_thickness;
+const glassCubeDepth = maze.depth * maze.cell_size + 20 * maze.wall_thickness;
+
+// Create the glass box
+const glassMaterial = new THREE.MeshBasicMaterial({ transparent: true, opacity: 0.5 });
+const glassGeometry = new THREE.BoxGeometry(glassCubeWidth, glassCubeHeight, glassCubeDepth);
+const glassBox = new THREE.Mesh(glassGeometry, glassMaterial);
+scene.add(glassBox);
+
+// Set positions of the glass planes
+glassBox.position.set(0, 0, 0); // Adjust position as needed
+
+// Define collision shape for the glass planes
+const glassPlaneShape = new CANNON.Plane();
+
+// Define positions and orientations of the glass planes
+const planePositions = [
+    new CANNON.Vec3(0, 0, glassCubeDepth / 2),   // Front
+    new CANNON.Vec3(0, 0, -glassCubeDepth / 2),  // Back
+    new CANNON.Vec3(-glassCubeWidth / 2, 0, 0),  // Left
+    new CANNON.Vec3(glassCubeWidth / 2, 0, 0),   // Right
+    new CANNON.Vec3(0, glassCubeHeight / 2, 0),  // Top
+    new CANNON.Vec3(0, -glassCubeHeight / 2, 0)  // Bottom
+];
+const planeQuaternions = [
+    new CANNON.Quaternion().setFromEuler(0, Math.PI, 0), // Front
+    new CANNON.Quaternion(), // Back
+    new CANNON.Quaternion().setFromEuler(0, Math.PI/2, 0), // Left
+    new CANNON.Quaternion().setFromEuler(0, -Math.PI/2, 0), // Right
+    new CANNON.Quaternion().setFromEuler( Math.PI/2,0 , 0), // Top
+    new CANNON.Quaternion().setFromEuler(-Math.PI/2, 0, 0), // Bottom
+    // new CANNON.Quaternion().setFromAxisAngle(new CANNON.Vec3(0, 1, 0), Math.PI / 2), // Left
+    // new CANNON.Quaternion().setFromAxisAngle(new CANNON.Vec3(0, 1, 0), -Math.PI / 2), // Right
+    // new CANNON.Quaternion().setFromAxisAngle(new CANNON.Vec3(1, 0, 0), Math.PI / 2), // Top
+    // new CANNON.Quaternion().setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2) // Bottom
+];
 
 
+// Add glass planes to the world
+for (let i = 0; i < planePositions.length; i++) {
+    const glassPlane = new CANNON.Body({
+        mass: 0,
+        shape: glassPlaneShape,
+        material: new CANNON.Material({
+            friction: 0,
+            restitution: 0
+        }),
+        position: planePositions[i],
+        quaternion: planeQuaternions[i],
+    });
+    
+    world.addBody(glassPlane);
+}
 
 // Wall bodies
 const wallBodies = []
@@ -163,7 +229,7 @@ function extractVerticesAndIndices(geometry) {
 // Assign a collision body for each wall
 maze.walls.forEach(wall => {
     
-    // Get width, height, depth for each wall
+    // DONOT REMOVE: need for testing because for now it is faster
     // wall.updateMatrixWorld();
     // const bbox = new THREE.Box3().setFromObject(wall);
     // const width = bbox.max.x - bbox.min.x;
@@ -173,8 +239,8 @@ maze.walls.forEach(wall => {
     // const wallShape = new CANNON.Box(new CANNON.Vec3(width/2, height/2, depth/2)); // not sure about this if Box is enough or it should be convex polyhedron
 
 
+    // THE REAL WORKING ONE: very slow for now
     // Create the wall shape based on the width, height and depth of the wall
-    // ConvexPolyhedron takes in: points - list of all vertices (?) and faces - array of integer arrays, describes which vertices is included in each face
     const wallData = extractVerticesAndIndices(wall.geometry);
     let vertices = wallData.vertices
     let indices = wallData.indices
@@ -183,7 +249,6 @@ maze.walls.forEach(wall => {
 
 
     // Create wall body with shape, material, mass, position and quaternion as properties
-    
     const wallBody = new CANNON.Body({ 
         shape: wallShape,
         material: new CANNON.Material({
@@ -200,20 +265,11 @@ maze.walls.forEach(wall => {
     wallBodies.push(wallBody);
 })
 
-world.allowSleep = true; // improve performance
-world.defaultContactMaterial.friction = 1; 
-
-
-
+// Adjust this value as needed; cannot be extra big may pass throguh walls
+const moveForce = 10
 // Move the ball
-const moveForce = 1; 
-
-const moveDistance = 0.01
-
-//WARNING: DOES NOT WORK Move the ball using force
 function handleKeyDown(event) {
-    const keyCode = event.keyCode;
-    const moveForce = 1; // Adjust this value as needed
+    const keyCode = event.keyCode; 
 
     // Apply force based on the key pressed
     ballBody.wakeUp()
@@ -233,36 +289,21 @@ function handleKeyDown(event) {
     }
 }
 
-// function handleKeyDown(event){
-//     const keyCode = event.keyCode;
-//     switch (keyCode) {
-//         case 87: // W
-//             ballBody.position.y += moveDistance
-//             break;
-//         case 65: // A
-//             ballBody.position.x -= moveDistance
-//             break;
-//         case 83: // S
-//             ballBody.position.y -= moveDistance
-//             break;
-//         case 68: // D
-//             ballBody.position.x += moveDistance
-//             break;
-//     }
-// }
 
 
 function updateWallAndBallPositionAndQuaternion(){
     ball.position.copy(ballBody.position);
     ball.quaternion.copy(ballBody.quaternion);
+
     // console.log('Ball Mesh Position: ', ball.position)
     // console.log('Ball Body Position: ', ballBody.position)
-    maze.walls.forEach((wall, index) =>{
-        if(wallBodies[index]){
-            wall.position.copy(wallBodies[index].position);
-            wall.quaternion.copy(wallBodies[index].quaternion);
-        }
-    })
+    // maze.walls.forEach((wall, index) =>{
+    //     if(wallBodies[index]){
+    //         wall.position.copy(wallBodies[index].position);
+    //         wall.quaternion.copy(wallBodies[index].quaternion);
+    //     }
+    // })
+    
 }
 
 
@@ -274,7 +315,6 @@ document.addEventListener('keydown', handleKeyDown, false);
 function animate() {
 	requestAnimationFrame( animate );
     world.step(1/60); // Step the physics world
-    
     
     updateWallAndBallPositionAndQuaternion();
     
